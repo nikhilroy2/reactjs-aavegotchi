6f88bb052c0840edb3d52e57eaf4ef3712d3135c
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2022-02-27 16:12:42 +0600

    portalclosedjson done

--- src/Pages/PortalClosed/PortalClosed.js
+++ src/Pages/PortalClosed/PortalClosed.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./PortalClosed.css";
 function PortalClosed(props) {
   const asideLinkObject = [
@@ -51,18 +51,28 @@ function PortalClosed(props) {
       icon_img: "https://aavegotchi.com/images/icons/activity.svg",
     },
   ];
+  const [portalTabList, setPortalTabList] = useState([]);
+  useEffect(() => {
+    fetch("./PortalClosedJson.json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.data);
+        setPortalTabList(data.data);
+      })
+      .catch((e) => console.log(e));
+  }, []);
   return (
     <div id="PortalClosed">
-      <div className="row g-0">
+      <div className="row g-0 mt-5">
         <div className="w-25 col_size_wrapper">
-          <aside className="p-3">
+          <aside className="p-3 position-sticky" style={{ top: "0px" }}>
             <h3 className="box_title text-center">Collections</h3>
             <ul className="list-unstyled aside_link_list">
               {asideLinkObject.map((v) => {
                 return (
                   <li key={v.id}>
                     <a
-                      className={`d-flex align-items-center p-2 ${
+                      className={`d-flex align-items-center p-3 ${
                         v.name == "PORTALS - CLOSED" && "active_link"
                       }`}
                       href={v.link}
@@ -82,21 +92,155 @@ function PortalClosed(props) {
         </div>
         <div className="w-75 content_right_side col_size_wrapper">
           <div className="content_wrapper">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Repellendus voluptatem quos soluta molestiae adipisci tenetur
-              similique aut quasi totam est quae in accusantium voluptatum
-              tempora eum expedita optio sapiente debitis dolorem ex, nesciunt
-              pariatur saepe! Consectetur ipsum possimus pariatur tempore et,
-              necessitatibus cumque. Incidunt voluptatibus est commodi!
-              Veritatis totam blanditiis, a enim soluta saepe aut aperiam
-              explicabo nobis eos, dignissimos consequatur. Veritatis est rerum
-              blanditiis? Quisquam ex id voluptates optio quod consectetur
-              distinctio dolorum tempora quo aperiam tenetur corporis dolores
-              laborum, neque provident, vero accusamus a esse impedit aspernatur
-              necessitatibus cum. Laboriosam inventore iusto animi, accusantium
-              facere labore! Molestiae, corrupti!
-            </p>
+            <div id="portalClosedContent">
+              <ul
+                className="list-unstyled d-flex tabItemWrapper mb-0 position-sticky"
+                style={{ top: "0px" }}
+              >
+                <li>PORTALS-CLOSED</li>
+                <li>50 RESULTS</li>
+
+                <li className="list_dropdown dropdown">
+                  <button
+                    className="dropdown-toggle"
+                    data-mdb-toggle="dropdown"
+                  >
+                    Sort: Lowest Price
+                  </button>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="dropdownMenuButton"
+                  >
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Latest
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Lowest price
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Highest price
+                      </a>
+                    </li>
+
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        tokenid (ASC)
+                      </a>
+                    </li>
+
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        tokenid (desc)
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+
+                <li className="list_dropdown dropdown">
+                  <button
+                    className="dropdown-toggle"
+                    data-mdb-toggle="dropdown"
+                  >
+                    Haunt: all
+                  </button>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="dropdownMenuButton"
+                  >
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        ALL
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        HAUNT 1
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        HAUNT 2
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+
+              <div className="tabItemContent">
+                {portalTabList.length === 0 ? (
+                  <ul className="list-unstyled mb-0 tabItemList">
+                    <li>
+                      <h3>Loading listings...</h3>
+                    </li>
+                  </ul>
+                ) : (
+                  <ul className="list-unstyled mb-0 tabItemList">
+                    {portalTabList.map((v) => {
+                      return (
+                        <li key={v.id}>
+                          <a href="#" className="d-block tabContentLink">
+                            <div className="row">
+                              <div className="col-xl-3 col-lg-4 col-md-4 col-sm-4 col-12">
+                                <img
+                                  src={
+                                    require("../../Static/img/icon_img/h2_closed_img.svg")
+                                      .default
+                                  }
+                                  alt="img"
+                                />
+                              </div>
+
+                              <div className="col-xl-9 col-lg-8 col-md-8 col-sm-8 col-12">
+                                <div className="col_wrapper">
+                                  <div className="col_header">
+                                    <div className="d-flex align-items-center justify-content-between">
+                                      <h3 className="mb-0">
+                                        UNOPENED PORTAL 21110
+                                      </h3>
+
+                                      <span>
+                                        <img
+                                          className="me-2"
+                                          style={{
+                                            width: "25px",
+                                            height: "25px",
+                                          }}
+                                          src={require("../../Static/img/icon_img/ghsttoken.png")}
+                                          alt=""
+                                        />
+                                        672
+                                      </span>
+                                    </div>
+                                  </div>
+                                  <div className="col_body">
+                                    <ul className="list-unstyled">
+                                      <li className="text-uppercase">
+                                        Haunt: 2
+                                      </li>
+                                      <li className="text-uppercase">
+                                        never sold
+                                      </li>
+                                      <li>
+                                        Owner: <a href="#">0x70...8f51</a>{" "}
+                                      </li>
+                                    </ul>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </a>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                )}
+              </div>
+            </div>
           </div>
         </div>
       </div>
